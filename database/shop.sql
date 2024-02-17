0f6128d4595e431bc2b6eb25b2814396b937df50
kaerz999ps <57294366+nomadsdev@users.noreply.github.com>
2024-02-18 03:50:33 +0700

    Web Shop not a php

---
+++ database/shop.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2024 at 09:24 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `codes`
+--
+
+CREATE TABLE `codes` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `points` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image_url`) VALUES
+(1, 'Valorant', 199.00, 'Hack Valorant', 'https://placehold.co/300x300/EEE/31343C'),
+(2, 'FiveM', 599.00, 'Winterfell', 'https://placehold.co/300x300/EEE/31343C'),
+(4, 'jmmstudio', 999.00, 'jmmstudio', 'https://placehold.co/300x300/EEE/31343C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL,
+  `point` int(11) DEFAULT 0,
+  `code` varchar(255) DEFAULT NULL,
+  `code_point` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `point`, `code`, `code_point`) VALUES
+(1, 'admin', 'admin', 'admin', 1000, 'Code123', NULL),
+(2, 'kukkai', 'kukkai', 'user', 700, NULL, 0),
+(3, 'user', 'user', 'user', 0, NULL, 0),
+(4, 'jmm', 'jmm', 'admin', 888, NULL, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `codes`
+--
+ALTER TABLE `codes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `codes`
+--
+ALTER TABLE `codes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `codes`
+--
+ALTER TABLE `codes`
+  ADD CONSTRAINT `codes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,338 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('express');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const serverrun = `Server is running ${PORT}`;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connection to database');
+    } else {
+        console.log('Connected to database');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.post('/', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const sqlLogin = `SELECT * FROM users WHERE username = ? AND password = ?`;
+
+    if (username && password) {
+        db.query(sqlLogin, [username, password], function(err, LoginResults, fields) {
+            if (LoginResults.length > 0) {
+                req.session.loggedin = true;
+                req.session.username = username;
+                req.session.role = LoginResults[0].role;
+                res.redirect('/home');
+            } else {
+                res.send('Invalid username or password');
+            }
+            res.end();
+        });
+    } else {
+        res.send('Please enter username and password');
+        res.end();
+    }
+});
+
+app.get('/home', (req, res) => {
+    if (req.session.loggedin) {
+        const sqlUser = `SELECT * FROM users WHERE username = ?`;
+        const sqlProducts = `SELECT * FROM products`;
+        db.query(sqlUser, [req.session.username], (err, userResults) => {
+            if (err) throw err;
+            db.query(sqlProducts, (err, products) => {
+                if (err) throw err;
+                res.render('home', { user: userResults[0], products });
+            })
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/product/:id', (req, res) => {
+    const productId = req.params.id;
+    const sqlProduct = `SELECT * FROM products WHERE id = ?`;
+    db.query(sqlProduct, [productId], (err, product) => {
+        if (err) {
+            console.error('Error retrieving product:', err);
+            res.send('Error retrieving product');
+            return;
+        }
+        res.render('productdis', { product });
+    });
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
+});
+
+app.get('/admin', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const sqlAllUsers = `SELECT * FROM users`;
+    const sqlProducts = `SELECT * FROM products`;
+    const sqlDisProduct = `SELECT * FROM products`;
+    db.query(sqlAllUsers, (err, allUsers) => {
+        if (err) {
+            console.error('Error retrieving users:', err);
+            res.send('Error retrieving users');
+            return;
+        }
+        db.query(sqlProducts, (err, products) => {
+            if (err) throw err;
+            db.query(sqlDisProduct, (err, totalProducts) => {
+                if (err) throw err;
+                res.render('admin_home', { users: allUsers, user: req.session.username, products, totalProducts: totalProducts });
+            })
+        })
+    });
+});
+
+app.post('/edit-role', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const username = req.body.username;
+    const newRole = req.body.role;
+    const sqlUpdateRole = `UPDATE users SET role = ? WHERE username = ?`;
+
+    db.query(sqlUpdateRole, [newRole, username], (err, result) => {
+        if (err) {
+            console.error('Error updating user role:', err);
+            res.send('Error updating user role');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/delete-user', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const username = req.body.username;
+    const sqlDeleteUser = `DELETE FROM users WHERE username = ?`;
+
+    db.query(sqlDeleteUser, [username], (err, result) => {
+        if (err) {
+            console.error('Error deleting user:', err);
+            res.send('Error deleting user');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/add-user', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const { username, password, role } = req.body;
+    const sqlAddUser = `INSERT INTO users (username, password, role) VALUES (?, ?, ?)`;
+
+    db.query(sqlAddUser, [username, password, role], (err, result) => {
+        if (err) {
+            console.error('Error adding user:', err);
+            res.send('Error adding user');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/add-point', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const { username, point } = req.body;
+    const sqlAddPoint = `UPDATE users SET point = point + ? WHERE username = ?`;
+
+    db.query(sqlAddPoint, [point, username], (err, result) => {
+        if (err) {
+            console.error('Error adding point:', err);
+            res.send('Error adding point');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/delete-point', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const { username } = req.body;
+    const sqlDeletePoint = `UPDATE users SET point = 0 WHERE username = ?`;
+
+    db.query(sqlDeletePoint, [username], (err, result) => {
+        if (err) {
+            console.error('Error deleting user point:', err);
+            res.send('Error deleting user point');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/add-product', (req, res) => {
+    if (!req.session.loggedin || req.session.role !== 'admin') {
+        res.redirect('/');
+        return;
+    }
+    const { name, price, description, image_url } = req.body;
+    const sqlAddProduct = `INSERT INTO products (name, price, description, image_url) VALUES (?, ?, ?, ?)`;
+
+    db.query(sqlAddProduct, [name, price, description, image_url], (err, result) => {
+        if (err) {
+            console.error('Error adding product:', err);
+            res.send('Error adding product');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/delete-product', (req, res) => {
+    const productId = req.body.product_id;
+    const sqlDeleteProduct = `DELETE FROM products WHERE id = ?`;
+    db.query(sqlDeleteProduct, [productId], (err, result) => {
+        if (err) {
+            console.error('Error deleting product:', err);
+            res.send('Error deleting product');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/update-product', (req, res) => {
+    const productId = req.body.product_id;
+    const productName = req.body.name;
+    const productPrice = req.body.price;
+    const productDescription = req.body.description;
+
+    const sqlUpdateProduct = `UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?`;
+
+    db.query(sqlUpdateProduct, [productName, productPrice, productDescription, productId], (err, result) => {
+        if (err) {
+            console.error('Error updating product:', err);
+            res.send('Error updating product');
+            return;
+        }
+        res.redirect('/admin');
+    });
+});
+
+app.post('/edit-product', (req, res) => {
+    const productId = req.body.product_id;
+    const sqlGetProduct = `SELECT * FROM products WHERE id = ?`;
+    db.query(sqlGetProduct, [productId], (err, result) => {
+        if (err) {
+            console.error('Error retrieving product:', err);
+            res.send('Error retrieving product');
+            return;
+        }
+        const product = result[0];
+        res.send(`
+            <head>
+                <script src="https://cdn.tailwindcss.com"></script>
+                <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+                <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@500&display=swap" rel="stylesheet">
+                <style>
+                    *{
+                        font-family: "Noto Sans Thai", sans-serif;
+                    }
+                </style>
+            </head>
+            <script>
+                document.addEventListener('DOMContentLoaded', () => {
+                    const editButton = document.getElementById('editButton');
+                    const modal = document.getElementById('editProductModal');
+                    const closeBtn = document.querySelector('.close');
+
+                    editButton.addEventListener('click', () => {
+                        modal.style.display = 'block';
+                    });
+
+                    closeBtn.addEventListener('click', () => {
+                        modal.style.display = 'none';
+                    });
+                });
+            </script>
+            <div class="flex justify-center px-5 py-20">
+                <div class="max-w-4xl w-full">
+                    <div id="editProductModal" class="modal border rounded-md shadow p-10 pb-5">
+                    <div class="modal-content">
+                        <div class="flex justify-between pb-2">
+                            <h2>แก้ไขข้อมูลสินค้า</h2>
+                            <span class="close">&times;</span>
+                        </div>
+                        <form action="/update-product" method="POST">
+                            <input type="hidden" name="product_id" value="${product.id}">
+                            <div class="pb-2">
+                                <label for="name">ชื่อสินค้า <span class="text-red-500">*</span></label><br>
+                                <input type="text" id="name" name="name" value="${product.name}" class="border rounded-md px-2 py-1 text-sm"><br>
+                            </div>
+                            <br>
+                            <div class="pb-2">
+                                <label for="price">ราคา <span class="text-red-500">*</span></label><br>
+                                <input type="number" id="price" name="price" value="${product.price}" class="border rounded-md px-2 py-1 text-sm"><br>
+                            </div>
+                            <br>
+                            <div class="pb-2">
+                                <label for="description">รายละเอียด <span class="text-red-500">*</span></label><br>
+                                <textarea id="description" name="description" class="border rounded-md px-2 py-1 text-sm">${product.description}</textarea><br>
+                            </div>
+                            <br>
+                            
+                            <button type="submit" class='border rounded-md w-full py-1 text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white transition'>อัพเดทข้อมูล</button>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(serverrun);
+});
